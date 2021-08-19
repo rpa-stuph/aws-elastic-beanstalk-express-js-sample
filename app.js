bfd07b23f911a6918fc618b3d45697ff16923617
Rich Averill <rich@Richs-MacBook-Pro.local>
2021-08-19 02:59:36 -0400

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Not another Hello World! message!'));
+app.get('/', (req, res) => res.send('Testing pipeline with a manual review stage.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
